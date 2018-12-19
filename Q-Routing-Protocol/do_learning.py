7c777fef06b00a18f8af74e140f2e5ff557c647e
Joshua Libin <jdl2193@columbia.edu>
2018-12-19 17:55:12 -0500

    Made a change to file dictionary extractor and create agents list that avoids printing the parameter file twice

--- Q-Routing-Protocol/do_learning.py
+++ Q-Routing-Protocol/do_learning.py
@@ -7,86 +7,91 @@ from utilities.helper_functions import prediction_file as pf
 
 
 def main(speak=True):
-    # The input parameter in the configuration path is now obsolete
+    interarrival_times = [1, 1.25, 2.5, 5, 7.5]
     d = fde('input_data/TestPar1.txt')
-    done, data, reward_history = False, [], []
+    for x in range(len(interarrival_times)):
+        done, data, reward_history = False, [], []
 
-    environment = NetworkSimulatorEnv()
-    environment.reset_env()
+        environment = NetworkSimulatorEnv()
+        environment.reset_env()
 
-    # --> Removing the next 6 lines for some reason results in all zero or Nan output
-    # Requests enter network according to a poisson distribution
-    environment.call_mean = d.get('interarrival_time')[0]
-    environment.cost = d.get('cost')[0]
-    environment.bbu_limit = d.get('resources_bbu')[0]
-    environment.edge_limit = d.get('resources_edge')[0]
+        # --> Removing the next 6 lines for some reason results in all zero or Nan output
+        # Requests enter network according to a poisson distribution
 
-    """Create list of 9 nodes each with two neural networks"""
-    agent_list = cal(d)
+        environment.call_mean = interarrival_times[x]
+        environment.cost = d.get('cost')[0]
+        environment.bbu_limit = d.get('resources_bbu')[0]
+        environment.edge_limit = d.get('resources_edge')[0]
 
-    for iteration in range(d.get('iterations')[0]):
-        print("PROCESSING ITERATION: {}\n".format(iteration))
+        """Create list of 9 nodes each with two neural networks"""
+        agent_list = cal(d)
 
-        """Reset environment for each epoch, and record startTime of epoch"""
-        node_destination_tuples = environment.reset_env()
-        started = datetime.now()
+        for iteration in range(d.get('iterations')[0]):
+            # if iteration == 1000:
+                # environment.call_mean = interarrival_times[]
 
-        for step in range(d.get('time_steps')[0]):
-            # UNLESS TERMINAL STATE REACHED
-            if not done:
-                current_node_destination_pair = node_destination_tuples[1]
-                current_node = current_node_destination_pair[0]
-                # Action is local edge
-                """Store the action in the Q Network agent"""
-                action = agent_list[current_node][0].act_nn2(environment.resources_edges, environment.resources_bbu)
+            print("PROCESSING ITERATION: {}\n".format(iteration))
 
-                """Store the environment state and action"""
-                agent_list[current_node][1].store_transition_temp(environment.resources_edges+environment.resources_bbu)
+            """Reset environment for each epoch, and record startTime of epoch"""
+            node_destination_tuples = environment.reset_env()
+            started = datetime.now()
 
-                """Execute action and store information tracking the request for next iteration"""
-                node_destination_tuples, done = environment.step(action)
+            for step in range(d.get('time_steps')[0]):
+                # UNLESS TERMINAL STATE REACHED
+                if not done:
+                    current_node_destination_pair = node_destination_tuples[1]
+                    current_node = current_node_destination_pair[0]
+                    # Action is local edge
+                    """Store the action in the Q Network agent"""
+                    action = agent_list[current_node][0].act_nn2(environment.resources_edges, environment.resources_bbu)
 
-                # EVERY 50 STEPS RECORD STATISTICS
-                if step % d.get('dumps')[0] == 0 and step > 0:
-                    """Calculate reward every dumps size, and track reward history"""
-                    reward = environment.calculate_reward()
-                    reward_history.append(reward)
-                    history_queue_length = len(environment.history_queue)
-                    current_information = [iteration, step, history_queue_length, environment.send_fail, reward]
-                    data.append(list(current_information))
+                    """Store the environment state and action"""
+                    agent_list[current_node][1].store_transition_temp(environment.resources_edges+environment.resources_bbu)
 
-                    if speak:
-                        print(current_information)
+                    """Execute action and store information tracking the request for next iteration"""
+                    node_destination_tuples, done = environment.step(action)
 
-                    environment.reset_history()
+                    # EVERY 50 STEPS RECORD STATISTICS
+                    if step % d.get('dumps')[0] == 0 and step > 0:
+                        """Calculate reward every dumps size, and track reward history"""
+                        reward = environment.calculate_reward()
+                        reward_history.append(reward)
+                        history_queue_length = len(environment.history_queue)
+                        current_information = [iteration, step, history_queue_length, environment.send_fail, reward]
+                        data.append(list(current_information))
 
-                    """Calculate loss for each node"""
-                    for node in range(0, environment.total_nodes):
-                        if node not in environment.bbu_connected_nodes:
-                            agent_list[node][0].store_transition_episode(reward)
-                            agent_list[node][1].store_transition_episode(reward)
+                        if speak:
+                            print(current_information)
 
-        print("Completed in", datetime.now() - started)
+                        environment.reset_history()
 
-        # (routed_packets, send fails, average number of hops, average completion time, max completion time)
+                        """Calculate loss for each node"""
+                        for node in range(0, environment.total_nodes):
+                            if node not in environment.bbu_connected_nodes:
+                                agent_list[node][0].store_transition_episode(reward)
+                                agent_list[node][1].store_transition_episode(reward)
 
-        # LEARN
-        learning = []
-        if iteration % 1 == 0:
-            for j in range(0, environment.total_nodes):
-                if j not in environment.bbu_connected_nodes:
-                    # agent_list[j].learn_val(iteration)
-                    agent_list[j][1].learn_val(iteration)
-                    val_approx = agent_list[j][1].eval_nn(environment.resources_edges, environment.resources_bbu)
-                    # print("node:{}, val_approx: {}".format(j, val_approx))
-                    agent_list[j][0].learn5(iteration, val_approx)
-                    if speak:
-                        learning.append(j)
-            if speak:
-                print('learning:', learning, '\n')
+            print("Completed in", datetime.now() - started)
 
-    data = np.array('results', data)
-    pf(data)
+            # (routed_packets, send fails, average number of hops, average completion time, max completion time)
+
+            # LEARN
+            learning = []
+            if iteration % 1 == 0:
+                for j in range(0, environment.total_nodes):
+                    if j not in environment.bbu_connected_nodes:
+                        # agent_list[j].learn_val(iteration)
+                        agent_list[j][1].learn_val(iteration)
+                        val_approx = agent_list[j][1].eval_nn(environment.resources_edges, environment.resources_bbu)
+                        # print("node:{}, val_approx: {}".format(j, val_approx))
+                        agent_list[j][0].learn5(iteration, val_approx)
+                        if speak:
+                            learning.append(j)
+                if speak:
+                    print('learning:', learning, '\n')
+
+        data = np.array(data)
+        pf('actor_critic_interarrival_%s_%s' % (interarrival_times[x], d.get('dumps')), data)
 
 
 if __name__ == '__main__':
