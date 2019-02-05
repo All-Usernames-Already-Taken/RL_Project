c92abbf0ef9deeabba68e0508f83705fd793bae8
Joshua Libin <jdl2193@columbia.edu>
2019-02-05 11:55:28 -0500

    Renamed files to precisely represent contents.
    Signed-off-by: Joshua Libin <jdl2193@columbia.edu>

---
+++ Q-Routing-Protocol/output_data/Original_Data/Read_Original.py
@@ -0,0 +1,47 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+#
+# def calculate_avg(matrix, avg_length):
+#     running_average, routed, dropped = [], [], []
+#
+#     percent_dropped = []
+#     for i in range(0, len(matrix[4]), avg_length):
+#         temp = np.mean(matrix[4][i:i + avg_length])
+#         running_average.append(temp)
+#         temp2 = np.sum(matrix[3][i:i + avg_length])
+#         routed.append(temp2)
+#         temp3 = np.sum(matrix[2][i:i + avg_length])
+#         dropped.append(temp3)
+#         percent_dropped.append(temp2 / (temp2 + temp3))
+#     return running_average, percent_dropped
+#
+# avg_length = 240
+# test_file1 = '/Users/JLibin/Desktop/prediction.txt'
+# a7 = np.loadtxt(test_file1, delimiter=',', usecols=range(5))
+# b7 = np.transpose(a7)
+# running_average7, percent_dropped7 = calculate_avg(b7, avg_length)
+#
+# # test_file2 = 'predictionsTestPar10'
+# # a8 = np.loadtxt(test_file2, delimiter=',', usecols=range(5))
+# # b8 = np.transpose(a8)
+# # running_average8, percent_dropped8 = calculate_avg(b8, avg_length)
+#
+# x_pts7 = np.linspace(1, len(running_average7), len(running_average7))
+#
+# x_pts8 = np.linspace(1, len(running_average8), len(running_average8))
+# plt.plot(x_pts7, running_average7, label='Test 1')
+# plt.plot(x_pts8, running_average8, label='Test 2')
+#
+# plt.xlabel('Iteration')
+# plt.ylabel('Average Reward')
+# plt.legend(loc='lower right')
+# plt.show()
+#
+# plt.plot(x_pts7, percent_dropped7, label='Test 1')
+# plt.plot(x_pts8, percent_dropped8, label='Test 2')
+#
+# plt.xlabel('Iteration')
+# plt.ylabel('Fraction Dropped ')
+# plt.legend(loc='upper right')
+# plt.show()
--- Q-Routing-Protocol/utilities/readPredictions.py
+++ Q-Routing-Protocol/utilities/readPredictions.py
@@ -1,30 +1,26 @@
-import numpy as np
 from numpy import transpose as tpose
 from numpy import loadtxt as ldtxt
-from numpy import linspace as lnspc
-import matplotlib.pyplot as plt
 from pylab import *
-from pprint import pprint
 from os import listdir as ld
 from os.path import join
-from os import getcwd, chdir
+import numpy as np
 
-
-def calculate_avg(matrix, avg_len):
-    running_Avg = []
+def calculate_avg(mtrx, avg_leng):
+    running_avg = []
     routed = []
     dropped = []
+    frac_dropped = []
 
-    percent_dropped = []
-    for i in range(0, len(matrix[4]), avg_len):
-        temp = np.mean(matrix[4][i:i + avg_len])
-        running_Avg.append(temp)
-        temp2 = np.sum(matrix[3][i:i + avg_len])
+    for i in range(0, len(mtrx[4]), avg_leng):
+        temp = np.mean(mtrx[4][i:i + avg_leng])
+        running_avg.append(temp)
+        temp2 = np.sum(mtrx[3][i:i + avg_leng])
         routed.append(temp2)
-        temp3 = np.sum(matrix[2][i:i + avg_len])
+        temp3 = np.sum(mtrx[2][i:i + avg_leng])
         dropped.append(temp3)
-        percent_dropped.append(temp2 / (temp2 + temp3))
-    return (running_Avg, percent_dropped)
+        fraction_dropped.append( temp2/(temp2 + temp3) )
+    return running_avg, frac_dropped
+
 
 number_of_subplots = 2
 avg_len = 60
@@ -37,13 +33,13 @@ rltf = len(tf)
 transposed = [tpose(ldtxt(tf[f], delimiter=',', usecols=range(5))) for f in range(rltf)]
 calc_averages = [calculate_avg(transposed[t], avg_len) for t in range(rltf)]
 running_averages = [calc_averages[n][0] for n in range(rltf)] 
-percent_dropped = [calc_averages[n][1] for n in range(rltf)]
+fraction_dropped = [calc_averages[n][1] for n in range(rltf)]
 pts = [linspace(1, len(running_averages[n]), len(running_averages[n])) for n in range(rltf)]
 
 fig = plt.figure()
 ax1 = plt.subplot(2, 1, 1)
 for n in range(rltf):
-	ax1.plot(pts[0], running_averages[n], label='%s' % tf1[n][:-10])
+    ax1.plot(pts[0], running_averages[n], label='%s' % tf1[n][:-10])
 ax1.set_ylabel('Average Reward', fontsize=8)	
 ax1.set_xlabel('Iteration', fontsize=8)	
 ax1.set_title("Average Reward Learning Curve", fontsize=10)
@@ -51,7 +47,7 @@ ax1.legend(loc='lower right', fontsize=8, frameon=False)
 
 ax2 = plt.subplot(2, 1, 2)
 for n in range(rltf):
-	ax2.plot(pts[0], percent_dropped[n], label='%s' % tf1[n][:-10])
+    ax2.plot(pts[0], fraction_dropped[n], label='%s' % tf1[n][:-10])
 ax2.legend(loc='upper right', fontsize=8, frameon=False)	
 ax2.set_ylabel('Fraction of Requests Dropped', fontsize=8)	
 ax2.set_xlabel('Iteration', fontsize=8)	
@@ -59,12 +55,3 @@ ax2.set_title("Jobs Dropped Learning Curve", fontsize=10)
 
 fig.subplots_adjust(hspace=0.5)
 plt.show()
-
-respo = input('save?: ')
-if respo == 'y':
-	chdir('/Users/JLibin/Desktop')
-	file_name = tf1[n][:-10]
-	file_name = file_name[3:]
-	savefig('%s' % file_name)
-	
-	
